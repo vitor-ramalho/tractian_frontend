18a1f86acdaa5e0b70081110a553422b4c857972
{"nickname": "JSON"} <39497088+vitor-ramalho@users.noreply.github.com>
2023-04-06 19:01:21 -0300

    Add Router configuration and Pages
    Add react router dom configuration and links to the pages.

---
+++ src/Components/Workorders/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Workorders = () => {
+  return (
+    <div>Workorders</div>
+  )
+}
+
+export default Workorders
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { router } from './router/router'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
 )
